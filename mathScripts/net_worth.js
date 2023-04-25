90eff864a0c242d0041e7644237bc0f372489c53
Wendy Flores <wendyf@appdev.yearup.org>
2023-04-25 10:46:57 -0400

    First commit

---
+++ mathScripts/net_worth.js
@@ -0,0 +1,7 @@
+var assets = 2000;
+var debts = -50;
+
+var netWorth = assets + debts;
+
+console.log('Your net worth is: ', netWorth);
+
---
+++ tips.js
@@ -0,0 +1,10 @@
+var foodCost = 79.25;
+var tax = 6.54;
+var tip = 12.0;
+
+var totalDue =foodCost + tax + tip;
+
+//print output
+console.log('Food cost is', foodCost, 'and tax is', tax);
+console.log('Tip is', tip);
+console.log('total due is', totalDue);
